d4830a691fef76f0b167a142269e04cfb333f38e
huzsaif <huzaifaxsaifee@gmail.com>
2025-04-02 12:04:57 -0400

    Add SPA routing configurations for Render and other platforms

---
+++ server.cjs
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the dist directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// For any request that doesn't match a static file,
+// send the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); 
